b86873edfec78d91fa2818cb3fe1b965201096ae
FMWcatcat <nikomakela15@gmail.com>
2025-05-06 09:39:38 +0300

    updates

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,47 +1,96 @@
 import unittest
+import sys
+import os
+from textnode import TextNode, TextType
+from textnode import split_nodes_image, split_nodes_link, text_to_textnodes
 
-from textnode import TextNode, TextType, split_nodes_image, split_nodes_link
-
-
-class TestTextNode(unittest.TestCase):
-    def test_eq(self):
-        node = TextNode("This is a text node", TextType.BOLD)
-        node2 = TextNode("This is a text node", TextType.BOLD)
-        self.assertEqual(node, node2)
+class TestTextToTextNodes(unittest.TestCase):
+    def test_text_to_textnodes_simple(self):
+        text = "Hello, world!"
+        nodes = text_to_textnodes(text)
+        self.assertEqual(len(nodes), 1)
+        self.assertEqual(nodes[0].text, "Hello, world!")
+        self.assertEqual(nodes[0].text_type, TextType.TEXT)
+        self.assertIsNone(nodes[0].url)
     
-    def test_eq3(self):
-        node = TextNode("This is a not text node", TextType.BOLD)
-        node2 = TextNode("This is a text node", TextType.BOLD)
-        self.assertNotEqual(node, node2)
+    def test_text_to_textnodes_bold(self):
+        text = "Hello, **bold** world!"
+        nodes = text_to_textnodes(text)
+        self.assertEqual(len(nodes), 3)
+        self.assertEqual(nodes[0].text, "Hello, ")
+        self.assertEqual(nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(nodes[1].text, "bold")
+        self.assertEqual(nodes[1].text_type, TextType.BOLD)
+        self.assertEqual(nodes[2].text, " world!")
+        self.assertEqual(nodes[2].text_type, TextType.TEXT)
+    
+    def test_text_to_textnodes_italic(self):
+        text = "Hello, _italic_ world!"
+        nodes = text_to_textnodes(text)
+        self.assertEqual(len(nodes), 3)
+        self.assertEqual(nodes[0].text, "Hello, ")
+        self.assertEqual(nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(nodes[1].text, "italic")
+        self.assertEqual(nodes[1].text_type, TextType.ITALIC)
+        self.assertEqual(nodes[2].text, " world!")
+        self.assertEqual(nodes[2].text_type, TextType.TEXT)
 
-    def test_eq2(self):
-        node = TextNode("This is a text node", TextType.ITALIC)
-        node2 = TextNode("This is a text node", TextType.BOLD)
-        self.assertNotEqual(node, node2)
-
-    def test_eq4(self):
-        node = TextNode("This is a text node", TextType.BOLD, "http://car.com")
-        node2 = TextNode("This is a text cat", TextType.BOLD, "http://cat.com")
-        self.assertNotEqual(node, node2)
-
-def test_split_images(self):
-    node = TextNode(
-        "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
-        TextType.TEXT,
-    )
-    new_nodes = split_nodes_image([node])
-    self.assertListEqual(
-        [
-            TextNode("This is text with an ", TextType.TEXT),
-            TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
-            TextNode(" and another ", TextType.TEXT),
-            TextNode(
-                "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
-            ),
-        ],
-        new_nodes,
-    )
-
+    def test_text_to_textnodes_code(self):
+        text = "Hello, `code` world!"
+        nodes = text_to_textnodes(text)
+        self.assertEqual(len(nodes), 3)
+        self.assertEqual(nodes[0].text, "Hello, ")
+        self.assertEqual(nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(nodes[1].text, "code")
+        self.assertEqual(nodes[1].text_type, TextType.CODE)
+        self.assertEqual(nodes[2].text, " world!")
+        self.assertEqual(nodes[2].text_type, TextType.TEXT)
+    
+    def test_text_to_textnodes_image(self):
+        text = "Hello, ![alt text](https://example.com/image.png) world!"
+        nodes = text_to_textnodes(text)
+        self.assertEqual(len(nodes), 3)
+        self.assertEqual(nodes[0].text, "Hello, ")
+        self.assertEqual(nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(nodes[1].text, "alt text")
+        self.assertEqual(nodes[1].text_type, TextType.IMAGE)
+        self.assertEqual(nodes[1].url, "https://example.com/image.png")
+        self.assertEqual(nodes[2].text, " world!")
+        self.assertEqual(nodes[2].text_type, TextType.TEXT)
+    
+    def test_text_to_textnodes_link(self):
+        text = "Hello, [link text](https://example.com) world!"
+        nodes = text_to_textnodes(text)
+        self.assertEqual(len(nodes), 3)
+        self.assertEqual(nodes[0].text, "Hello, ")
+        self.assertEqual(nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(nodes[1].text, "link text")
+        self.assertEqual(nodes[1].text_type, TextType.LINK)
+        self.assertEqual(nodes[1].url, "https://example.com")
+        self.assertEqual(nodes[2].text, " world!")
+        self.assertEqual(nodes[2].text_type, TextType.TEXT)
+    
+    def test_text_to_textnodes_combined(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![image](https://example.com/image.png) and a [link](https://example.com)"
+        nodes = text_to_textnodes(text)
+        self.assertEqual(len(nodes), 10)
+        self.assertEqual(nodes[0].text, "This is ")
+        self.assertEqual(nodes[1].text, "text")
+        self.assertEqual(nodes[1].text_type, TextType.BOLD)
+        self.assertEqual(nodes[2].text, " with an ")
+        self.assertEqual(nodes[3].text, "italic")
+        self.assertEqual(nodes[3].text_type, TextType.ITALIC)
+        self.assertEqual(nodes[4].text, " word and a ")
+        self.assertEqual(nodes[5].text, "code block")
+        self.assertEqual(nodes[5].text_type, TextType.CODE)
+        self.assertEqual(nodes[6].text, " and an ")
+        self.assertEqual(nodes[7].text, "image")
+        self.assertEqual(nodes[7].text_type, TextType.IMAGE)
+        self.assertEqual(nodes[7].url, "https://example.com/image.png")
+        self.assertEqual(nodes[8].text, " and a ")
+        self.assertEqual(nodes[9].text, "link")
+        self.assertEqual(nodes[9].text_type, TextType.LINK)
+        self.assertEqual(nodes[9].url, "https://example.com")
 
 if __name__ == "__main__":
     unittest.main()
